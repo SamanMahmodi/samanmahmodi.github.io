bbf2b79e1e512f208b0ba0fcf6f58061de7d9015
Saman <samanmahmodi1998@gmail.com>
2025-12-04 00:38:34 +0330

    Update webloog-comments.js set latest

--- latest-comments/webloog-comments.js
+++ latest-comments/webloog-comments.js
@@ -1,8 +1,13 @@
-const script = document.currentScript;
-let comments = script.getAttribute("data-comments") ? Number(script.getAttribute("data-comments")) : 5;
-const webloogBlogId = script.getAttribute("data-blog");
-const webloogTheme = script.getAttribute("data-theme") || "light";
-const _style = `
+// Blogfa Latest Comments Widget v1.0
+// Author: Saman Mahmudi (http://webloog.ir)
+// Released: 2025-12-04
+// Docs: http://webloog.ir/post/66
+try {
+  const script = document.currentScript;
+  let comments = script.getAttribute("data-comments") ? Number(script.getAttribute("data-comments")) : 5;
+  const webloogBlogId = script.getAttribute("data-blog");
+  const webloogTheme = script.getAttribute("data-theme") || "light";
+  const _style = `
 <style>
   [data-theme="light"] {
     --body: #ffffff;
@@ -46,13 +51,6 @@ const _style = `
   --text: #2f2144;
   --muted: #7f6a9c;
 }
-[data-theme="cyber"] {
-  --body: #0e0f17;
-  --border: #24243a;
-  --link: #00ffe5;
-  --text: #d7fdf8;
-  --muted: #8fbab3;
-}
 [data-theme="rosegold"] {
   --body: #fff8f9;
   --border: #ffdce2;
@@ -88,7 +86,6 @@ const _style = `
   --text: #3b2f1b;
   --muted: #7d6a4c;
 }
-
   .webloog-wrap * {
     box-sizing: border-box;
     font-size: 10pt!important;
@@ -226,23 +223,21 @@ const _style = `
     padding:20px;
     text-align:center;
   }
-    .webloog-skeleton {
+  .webloog-skeleton {
   display: flex;
   flex-direction: row;
   gap: 10px;
   padding: 20px;
   border-bottom: 1px solid var(--border);
-  animation: pulse 1.8s infinite ease-in-out;
+  animation: pulse 2s infinite ease-in-out;
 }
-
 .webloog-skeleton-avatar {
   width: 40px;
   height: 40px;
   border-radius: 50%;
-  background: color-mix(in srgb, var(--border) 60%, transparent);
+  background: color-mix(in srgb, var(--border) 80%, transparent);
   flex-shrink: 0;
 }
-
 .webloog-skeleton-lines {
   width: 100%;
   display: flex;
@@ -253,18 +248,21 @@ const _style = `
 
 .webloog-skeleton-line {
   height: 10px;
-  width: 80%;
-  background: color-mix(in srgb, var(--border) 60%, transparent);
+  width: 60%;
+  background: color-mix(in srgb, var(--border) 80%, transparent);
   border-radius: 6px;
 }
-
+.webloog-skeleton-line:last-of-type {
+  width: 80%;
+  height:20px;
+}
 .webloog-skeleton-line.short {
   width: 40%;
 }
 
 </style>
 `;
-const _dom = `
+  const _dom = `
 <div class="webloog-wrap" dir="rtl" lang="fa" data-theme="${webloogTheme}">
   <div class="webloog-heading">
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.0867 21.3877L13.6288 20.4718C14.0492 19.7614 14.2595 19.4062 14.5972 19.2098C14.9349 19.0134 15.36 19.0061 16.2104 18.9915C17.4658 18.9698 18.2531 18.8929 18.9134 18.6194C20.1386 18.1119 21.1119 17.1386 21.6194 15.9134C22 14.9946 22 13.8297 22 11.5V10.5C22 10.1303 22 9.78153 21.9989 9.45187C21.9976 9.02454 21.4751 8.78307 21.0971 8.9825C20.4709 9.31294 19.7573 9.5 19 9.5C16.5147 9.5 14.5 7.48528 14.5 5C14.5 4.24271 14.6871 3.52911 15.0175 2.90288C15.2169 2.52494 14.9755 2.00244 14.5481 2.00106C14.2185 2 13.8697 2 13.5 2H10.5C7.22657 2 5.58985 2 4.38751 2.7368C3.71473 3.14908 3.14908 3.71473 2.7368 4.38751C2 5.58985 2 7.22657 2 10.5V11.5C2 13.8297 2 14.9946 2.3806 15.9134C2.88807 17.1386 3.86144 18.1119 5.08658 18.6194C5.74689 18.8929 6.53422 18.9698 7.78958 18.9915C8.63992 19.0061 9.06509 19.0134 9.40279 19.2098C9.74049 19.4063 9.95073 19.7614 10.3712 20.4718L10.9133 21.3877C11.3965 22.204 12.6035 22.204 13.0867 21.3877Z" fill="currentColor"/><circle cx="19" cy="5" r="3" fill="var(--link)"/></svg>
@@ -279,12 +277,12 @@ const _dom = `
   <div></div>
 </div>
 `;
-script.insertAdjacentHTML("beforebegin", _style + _dom);
-const webloogComments = document.querySelector(".webloog-comments");
-function createSkeletons(count) {
-  let out = "";
-  for (let i = 0; i < count; i++) {
-    out += `
+  script.insertAdjacentHTML("beforebegin", _style + _dom);
+  const webloogComments = document.querySelector(".webloog-comments");
+  function createSkeletons(count) {
+    let out = "";
+    for (let i = 0; i < count; i++) {
+      out += `
       <div class="webloog-skeleton">
         <div class="webloog-skeleton-avatar"></div>
         <div class="webloog-skeleton-lines">
@@ -294,32 +292,32 @@ function createSkeletons(count) {
         </div>
       </div>
     `;
+    }
+    return out;
   }
-  return out;
-}
-webloogComments.innerHTML = createSkeletons(comments);
-function replaceSkeletonWithComment(html) {
-  const firstSkeleton = webloogComments.querySelector(".webloog-skeleton");
-  if (firstSkeleton) {
-    firstSkeleton.outerHTML = html;
+  webloogComments.innerHTML = createSkeletons(comments);
+  function replaceSkeletonWithComment(html) {
+    const firstSkeleton = webloogComments.querySelector(".webloog-skeleton");
+    if (firstSkeleton) {
+      firstSkeleton.outerHTML = html;
+    }
   }
-}
-let foundComment = false;
-async function getComment(postId, page = 1) {
-  try {
-    const data = await fetch(`/comments/?blogid=${webloogBlogId}&postid=${postId}&p=${page}`);
-    const text = await data.text();
-    const dom = new DOMParser().parseFromString(text, "text/html");
-    const lastPage = dom.querySelector(`#navbar a[title="صفحه آخر"]`);
-    if (lastPage) {
-      await getComment(postId, lastPage.getAttribute("href").split("&=p")[1]);
-    } else {
-      const boxes = dom.querySelectorAll(".box");
-      const lastComment = boxes.length ? boxes[boxes.length - 1] : null;
-      if (lastComment) {
-        foundComment = true;
-        const author = lastComment.querySelector(".author");
-        const commentTemplate = `
+  let foundComment = false;
+  async function getComment(postId, page = 1) {
+    try {
+      const data = await fetch(`/comments/?blogid=${webloogBlogId}&postid=${postId}&p=${page}`);
+      const text = await data.text();
+      const dom = new DOMParser().parseFromString(text, "text/html");
+      const lastPage = dom.querySelector(`#navbar a[title="صفحه آخر"]`);
+      if (lastPage) {
+        await getComment(postId, lastPage.getAttribute("href").split("&=p")[1]);
+      } else {
+        const boxes = dom.querySelectorAll(".box");
+        const lastComment = boxes.length ? boxes[boxes.length - 1] : null;
+        if (lastComment) {
+          foundComment = true;
+          const author = lastComment.querySelector(".author");
+          const commentTemplate = `
          <div class="webloog-comment">
             <div class="webloog-line"></div>
             <div class="webloog-avatar" style="background:${randomComplexPattern()}">
@@ -342,79 +340,79 @@ async function getComment(postId, page = 1) {
             </div>
           </div>
         `;
-        replaceSkeletonWithComment(commentTemplate);
-        comments -= 1;
+          replaceSkeletonWithComment(commentTemplate);
+          comments -= 1;
+        }
       }
+    } catch (t) {
+      console.log(t);
     }
-  } catch (t) {
-    console.log(t);
   }
-}
+  function randomColor() {
+    // const colors = ["#ff6b6b", "#feca57", "#48dbfb", "#1dd1a1", "#5f27cd", "#ee5253", "#ff9ff3", "#54a0ff", "#00d2d3", "#f368e0", "#c8d6e5", "#576574", "#01a3a4", "#ff9f43", "#341f97"];
+    const colors = [
+      "#ffb3b3", // نرم‌شده‌ی ff6b6b
+      "#ffe4a8", // نرم‌شده‌ی feca57
+      "#aee9ff", // نرم‌شده‌ی 48dbfb
+      "#9ff3d3", // نرم‌شده‌ی 1dd1a1
+      "#b8a6ff", // نرم‌شده‌ی 5f27cd
+      "#ff9ca1", // نرم‌شده‌ی ee5253
+      "#ffc9ff", // نرم‌شده‌ی ff9ff3
+      "#a7caff", // نرم‌شده‌ی 54a0ff
+      "#8ff1f1", // نرم‌شده‌ی 00d2d3
+      "#ffb0f2", // نرم‌شده‌ی f368e0
+      "#e6eef5", // نرم‌شده‌ی c8d6e5
+      "#9da5ae", // نرم‌شده‌ی 576574
+      "#7ed6d6", // نرم‌شده‌ی 01a3a4
+      "#ffd29d", // نرم‌شده‌ی ff9f43
+      "#a89be6", // نرم‌شده‌ی 341f97
+    ];
 
-function randomColor() {
-  // const colors = ["#ff6b6b", "#feca57", "#48dbfb", "#1dd1a1", "#5f27cd", "#ee5253", "#ff9ff3", "#54a0ff", "#00d2d3", "#f368e0", "#c8d6e5", "#576574", "#01a3a4", "#ff9f43", "#341f97"];
-  const colors = [
-    "#ffb3b3", // نرم‌شده‌ی ff6b6b
-    "#ffe4a8", // نرم‌شده‌ی feca57
-    "#aee9ff", // نرم‌شده‌ی 48dbfb
-    "#9ff3d3", // نرم‌شده‌ی 1dd1a1
-    "#b8a6ff", // نرم‌شده‌ی 5f27cd
-    "#ff9ca1", // نرم‌شده‌ی ee5253
-    "#ffc9ff", // نرم‌شده‌ی ff9ff3
-    "#a7caff", // نرم‌شده‌ی 54a0ff
-    "#8ff1f1", // نرم‌شده‌ی 00d2d3
-    "#ffb0f2", // نرم‌شده‌ی f368e0
-    "#e6eef5", // نرم‌شده‌ی c8d6e5
-    "#9da5ae", // نرم‌شده‌ی 576574
-    "#7ed6d6", // نرم‌شده‌ی 01a3a4
-    "#ffd29d", // نرم‌شده‌ی ff9f43
-    "#a89be6", // نرم‌شده‌ی 341f97
-  ];
-
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function randomGradientLayer() {
-  const type = Math.random();
-  if (type < 0.4) {
-    // conic
-    const angle = Math.floor(Math.random() * 360);
-    const c1 = randomColor();
-    const c2 = randomColor();
-    const c3 = randomColor();
-    return `conic-gradient(from ${angle}deg, ${c1} 0%, ${c2} 50%, ${c3} 100%)`;
-  } else if (type < 0.7) {
-    // radial
-    const c1 = randomColor();
-    const c2 = randomColor() + "66"; // نیمه شفاف
-    const posX = Math.floor(Math.random() * 100);
-    const posY = Math.floor(Math.random() * 100);
-    return `radial-gradient(circle at ${posX}% ${posY}%, ${c1}, ${c2})`;
-  } else {
-    // linear thin lines
-    const angle = Math.floor(Math.random() * 360);
-    const c = randomColor() + "33"; // خیلی شفاف
-    return `repeating-linear-gradient(${angle}deg, ${c} 0px, ${c} 2px, transparent 2px, transparent 6px)`;
+    return colors[Math.floor(Math.random() * colors.length)];
   }
-}
-function randomComplexPattern(layers = 6) {
-  let gradients = [];
-  for (let i = 0; i < layers; i++) {
-    gradients.push(randomGradientLayer());
+  function randomGradientLayer() {
+    const type = Math.random();
+    if (type < 0.4) {
+      // conic
+      const angle = Math.floor(Math.random() * 360);
+      const c1 = randomColor();
+      const c2 = randomColor();
+      const c3 = randomColor();
+      return `conic-gradient(from ${angle}deg, ${c1} 0%, ${c2} 50%, ${c3} 100%)`;
+    } else if (type < 0.7) {
+      // radial
+      const c1 = randomColor();
+      const c2 = randomColor() + "66"; // نیمه شفاف
+      const posX = Math.floor(Math.random() * 100);
+      const posY = Math.floor(Math.random() * 100);
+      return `radial-gradient(circle at ${posX}% ${posY}%, ${c1}, ${c2})`;
+    } else {
+      // linear thin lines
+      const angle = Math.floor(Math.random() * 360);
+      const c = randomColor() + "33"; // خیلی شفاف
+      return `repeating-linear-gradient(${angle}deg, ${c} 0px, ${c} 2px, transparent 2px, transparent 6px)`;
+    }
   }
-  console.log(gradients.join(","));
-  return gradients.join(",");
-}
-randomComplexPattern();
-(async () => {
-  for (let index = 0; index < BlogComments.length && comments > 0; index += 2) {
-    await getComment(BlogComments[index]);
+  function randomComplexPattern(layers = 6) {
+    let gradients = [];
+    for (let i = 0; i < layers; i++) {
+      gradients.push(randomGradientLayer());
+    }
+    return gradients.join(",");
   }
-  if (!foundComment) {
-    webloogComments.innerHTML = `
+  randomComplexPattern();
+  (async () => {
+    for (let index = 0; index < BlogComments.length && comments > 0; index += 2) {
+      await getComment(BlogComments[index]);
+    }
+    if (!foundComment) {
+      webloogComments.innerHTML = `
       <div class="webloog-notFound">
         هیچ نظری یافت نشد
       </div>
     `;
-  }
-})();
+    }
+  })();
+} catch (error) {
+  console.log("خطایی در کد نظرات پیش آمده", error);
+}
